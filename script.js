7c322eef6f15d3f568cd435bd187a9270affa294
Jaydon <pearcejaydon@gmail.com>
2022-07-23 18:56:47 +1000

    Add loop to continue playing and count total wins

--- script.js
+++ script.js
@@ -2,22 +2,42 @@ const rock = 'Rock';
 const paper = 'Paper';
 const scissors = 'Scissors';
 
-let playerSelection = '';
-let computerSelection = getComputerChoice();
 
-playerSelection = prompt("Enter your selection: (Rock, Paper, Scissors");
 
+function game(){
+    let rounds = 0;
+    let score = 0;
+    let pChoice = '';
+    let cChoice = '';
+
+    for(let i = 0; i < 5; i++){
+        pChoice = getPlayerChoice();
+        cChoice = getComputerChoice();
+        score += playRound(pChoice, cChoice);
+        console.log(score);
+    }
+}
+
+function getPlayerChoice(){
+    let playerSelection = '';
+
+    playerSelection = prompt("Enter your selection: (Rock, Paper, Scissors");
+
+    //write code here to validate input is a rock or paper or scissor
+
+    return playerSelection;
+}
 function playRound(player, computer){
     if(player === computer){
-        return "Tie";
+        return 0;
     }else if (player === rock && computer === scissors){
-        return "Player Win!";
+        return 1;
     }else if (player === paper && computer === rock){
-        return "Player Win!";
+        return 1;
     }else if (player === scissors && computer === paper){
-        return "Player Win!";
+        return 1;
     }else {
-        return "Player Lose!";
+        return 0;
     }
 }
 function getComputerChoice(){
